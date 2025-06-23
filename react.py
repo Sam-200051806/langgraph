8f3ec6a75a9c951e79e6543bf9cf4a19fc7e5631
Sam-200051806 <sambhavseth1806@gmail.com>
2025-06-23 16:22:37 +0530

    done

---
+++ react.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_core.tools import tool
+from langchain_groq import ChatGroq
+from langchain_tavily import TavilySearch
+
+
+
+@tool
+def triple(num:float) -> float:
+    return float(num) * 3
+
+tools = [TavilySearch(max_results = 1),triple]
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0
+).bind_tools(tools)
